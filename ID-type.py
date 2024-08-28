ff9c3d5035072c8143a8a5c1921c441257c184d3
HusnainMughal338 <mughalhusnain437@gmail.com>
2024-08-28 07:33:45 +0000

    in it github

---
+++ ID-type.py
@@ -0,0 +1,15 @@
+print("hello world")
+
+#Find the id of any program.
+num01 = 12
+num02 = 13
+result = num01 + num02
+print("sum of these num is",result)
+
+
+#Find the id of these program.
+print(id(print))
+
+
+#Find the type of this program.
+print(type(result))
--- main.py
+++
@@ -1,8 +0,0 @@
-print("hello world")
-num1=10
-print(id(num1))
-devied=2//5
-print(devied)
-print(type(num1))
-result = 10 + 4 
-userInput = input("enter  number")
