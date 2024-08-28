45aea53b9b0e32839307f18dab0e0833373a65d9
HusnainMughal338 <mughalhusnain437@gmail.com>
2024-08-28 16:08:32 +0000

    init github

---
+++ hh.py
@@ -0,0 +1,31 @@
+#additon of two nums
+def addition(num1, num2):
+    num3 = num1 + num2
+    return num3
+result = addition(12,8)
+print(result)
+
+# multiplication of two nums.
+def multiplication(num1, num2):
+    num3 = num1 * num2
+    return num3
+result = multiplication(12,8)
+print(result)
+
+# subtraction of two nums.
+def subtraction(num1, num2):
+    num3 = num1 - num2
+    return num3
+result = subtraction(12,8)
+print(result)
+
+# division of two nums.
+def division(num1, num2):
+    num3 = num1 / num2
+    return num3
+result = division(12,8)
+print(result)
+
+
+
+
