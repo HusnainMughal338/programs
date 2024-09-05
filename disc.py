d2a9382d35ed4987a368bfa407f38fa0b2bd3bc1
HusnainMughal338 <mughalhusnain437@gmail.com>
2024-09-05 08:47:45 +0000

    submit to github

---
+++ disc.py
@@ -0,0 +1,7 @@
+def discriminent():
+    a = float(input("Enter the value of a:"))
+    b = float(input("Enter the value of b:"))
+    c = float(input("Enter the value of c:"))
+    print((b**2)-(4*a*c))
+    return a,b,c
+result = discriminent()
