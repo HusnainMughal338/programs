5303975807e68d2e8368c583fc0651c2d088c8c7
HusnainMughal338 <mughalhusnain437@gmail.com>
2024-09-09 09:24:35 +0000

    solve

---
+++ new.py
@@ -0,0 +1,17 @@
+num = int(input("Enter a number: "))
+count:int = 1
+while count <= 10:
+    print(f"{num} * {count} = {num * count}")
+    count += 1
+
+num = int(input("Enter a number: "))
+count = 1
+while count <= num:
+    print(count)
+    count = count + 1
+
+name = input("Enter your name: ")
+count:int = 1
+while count <= 1000:
+    print(name)
+    count = count + 1
